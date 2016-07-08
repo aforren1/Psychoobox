298c3b431a5f3c426e4c7fb505cdb09082b3865a
Alex Forrence <aforren1@jhu.edu>
2016-07-08 10:47:16 -0400

    Example documentation, doesn't work for octave?

--- classes/PsychHandle.m
+++ classes/PsychHandle.m
@@ -1,19 +1,33 @@
 classdef PsychHandle < handle
-% PsychHandle()
+% PsychHandle (Abstract) superclass for Psychoobox.
 % 
 % Provides generic getters/setters/closers. Does not do much more than that.
+%
+% PsychHandle Methods:
+%    Get - Get value of a field.
+%    Set - Set value of a field.
+%    Close - Delete handle.
 
     methods
 
         function value = Get(self, property)
+        % Get(property) Get the value of a property
+        %    Returns the value of a given field, passed in as a string.
+        %
+        %    See also SET.
             value = self.(property);
         end
 
         function Set(self, property, value)
+        % Set(property, value) Set the value of a property
+        %    Sets a field "property" to the value "value".
+        %
+        %    See also GET.
             self.(property) = value;
         end
 
         function Close(self)
+        % Close Delete the handle to the object.
             delete(self);
         end
 
