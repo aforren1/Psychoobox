9e8fae18a2db35b7d5d22e68af6b0ef473146310
Alex Forrence <aforren1@jhu.edu>
2016-07-10 19:53:36 -0400

    Remove inputParser from line storage, protected access seems off though

--- classes/Line.m
+++ classes/Line.m
@@ -6,21 +6,20 @@ classdef Line < PsychHandle
         to_h;
         to_v;
         pen_width;
-        p;
     end
 
     methods
         function self = Line(varargin)
-            self.p = inputParser;
-            self.p.FunctionName = 'Line';
-            self.p.addParamValue('color',  [], @(x) isempty(x) || isnumeric(x));
-            self.p.addParamValue('from_h', [], @(x) isempty(x) || isnumeric(x));
-            self.p.addParamValue('from_v', [], @(x) isempty(x) || isnumeric(x));
-            self.p.addParamValue('to_h', [], @(x) isempty(x) || isnumeric(x));
-            self.p.addParamValue('to_v', [], @(x) isempty(x) || isnumeric(x));
-            self.p.addParamValue('pen_width', 1, @(x) isnumeric(x) && x > 0);
-            self.p.parse(varargin{:});
-            opts = self.p.Results;
+            p = inputParser;
+            p.FunctionName = 'Line';
+            p.addParamValue('color',  [], @(x) isempty(x) || isnumeric(x));
+            p.addParamValue('from_h', [], @(x) isempty(x) || isnumeric(x));
+            p.addParamValue('from_v', [], @(x) isempty(x) || isnumeric(x));
+            p.addParamValue('to_h', [], @(x) isempty(x) || isnumeric(x));
+            p.addParamValue('to_v', [], @(x) isempty(x) || isnumeric(x));
+            p.addParamValue('pen_width', 1, @(x) isnumeric(x) && x > 0);
+            p.parse(varargin{:});
+            opts = p.Results;
             for fns = fieldnames(opts)'
                 self.(fns{1}) = opts.(fns{1});
             end
