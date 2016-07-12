65cac56860fdca16b2ccec8452d83282b4a6cafe
Alex Forrence <aforren1@jhu.edu>
2016-07-12 15:38:54 -0400

    Broken addition of DrawLines

--- classes/Line.m
+++ classes/Line.m
@@ -4,6 +4,10 @@ classdef Line < PsychHandle
         start_xy;
         stop_xy;
         pen_width;
+        smooth;
+
+        lenient;
+        center;
     end
 
     methods
@@ -11,9 +15,14 @@ classdef Line < PsychHandle
             p = inputParser;
             p.FunctionName = 'Line';
             p.addParamValue('color',  [], @(x) isempty(x) || isnumeric(x));
-            p.addParamValue('start_xy', [], @(x) isempty(x) || (isnumeric(x) && length(x) == 2));
-            p.addParamValue('stop_xy', [], @(x) isempty(x) || (isnumeric(x) && length(x) == 2));
+            p.addParamValue('start_xy', [], @(x) isempty(x) || isnumeric(x));
+            p.addParamValue('stop_xy', [], @(x) isempty(x) || isnumeric(x));
             p.addParamValue('pen_width', 1, @(x) isnumeric(x) && x > 0);
+            p.addParamValue('smooth', 0, @(x) any(x == 0:2));
+
+            p.addParamValue('lenient', 0, @(x) any(x == 0:1));
+            p.addParamValue('center', [], @(x) isempty(x) || dims(x) == dims([0 0]));
+
             p.parse(varargin{:});
             opts = p.Results;
             for fns = fieldnames(opts)'
@@ -22,8 +31,12 @@ classdef Line < PsychHandle
         end
 
         function Draw(self, pointer)
-            Screen('DrawLine', pointer, self.color, self.start_xy(1), self.start_xy(2),...
-                   self.stop_xy(1), self.stop_xy(2), self.pen_width);
+            if self.smooth > 0
+                Screen('DrawLines', pointer, )
+            else
+                Screen('DrawLine', pointer, self.color, self.start_xy(1), self.start_xy(2),...
+                       self.stop_xy(1), self.stop_xy(2), self.pen_width);
+            end
         end
 
     end
