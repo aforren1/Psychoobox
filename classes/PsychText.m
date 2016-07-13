6b5f62f117a8833e44d04c54d759a54382e52b85
Alex Forrence <aforren1@jhu.edu>
2016-07-13 15:30:18 -0400

    Added docs to PsychText

--- classes/PsychText.m
+++ classes/PsychText.m
@@ -1,4 +1,28 @@
 classdef PsychText < PsychHandle
+% PsychText Draw text on the specified window.
+%
+% Options available when `formatted = true` are marked with [DFT].
+% Settings under the default are marked with [DT].
+%
+% PsychText Properties:
+%    val - The string to be written.
+%    formatted - Use DrawFormattedText instead of DrawText. Default is false.
+%    size - Size of the text. Default is 10. [DT, DFT]
+%    style - Style of the font, including bold, italic, and underline. Default is 0 (normal). [DT, DFT]
+%    styles - Struct containing all possible styles. [DT, DFT]
+%    font - Default is Courier New. [DT, DFT]
+%    color - Default is 0 (black). [DT, DFT]
+%    background_color - Default is []. [DT]
+%    transform - 2x3 matrix specifying the text orientation. Default is the identity matrix. [DT, DFT]
+%    x - Position on x axis. Default is 0. 'center' is also allowed under DFT. [DT, DFT]
+%    y - Position on y axis. Default is 0. 'center' is also allowed under DFT. [DT, DFT]
+%    y_pos_is_baseline - Setting to 1 replicates old PTB behavior. [DT]
+%    right_to_left - Whether text should be written right to left. Defaults to 0 (false).
+%    renderer - Use new or old renderer. 1 (new) is default. [DT, DFT]
+%    wrapat - After this number of characters, automatically wrap the string. Default is []. [DFT]
+%    fliph - Flip text horizontally. Default is 0. [DFT]
+%    flipv - Flip text vertically. Default is 0. [DFT]
+%    vert_spacing - Add space between lines. Default is 1. [DFT]
 
     properties (SetAccess = public, GetAccess = public)
         size;
@@ -13,7 +37,6 @@ classdef PsychText < PsychHandle
         y;
         y_pos_is_baseline;
 
-        swap_text_direction;
         val;
         renderer;
         formatted;
@@ -79,7 +102,7 @@ classdef PsychText < PsychHandle
             else
                 Screen('DrawText', window_pointer, self.val, ...
                        self.x, self.y, self.color, self.background_color, ...
-                       self.y_pos_is_baseline, self.swap_text_direction);
+                       self.y_pos_is_baseline, self.right_to_left);
             end
         end
     end
