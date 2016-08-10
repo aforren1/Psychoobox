263610f1f43ab157239c392f6b56d79039af97ef
aforren1 <aforren1@jhu.edu>
2016-08-10 17:10:36 -0400

    Try to allow individual missing vals

--- RelativeToRect.m
+++ RelativeToRect.m
@@ -1,7 +1,8 @@
 function out_rect = RelativeToRect(rel_x_pos, rel_y_pos, ...
                                    rel_x_scale, rel_y_scale, ...
                                    reference_rect)
-
+% Use NaNs to allow individual missing vals
+%
     if any([isempty(rel_x_pos), isempty(rel_y_pos)])
        error('Must specify either rel_x_pos and rel_y_pos or rect.')
     end
@@ -10,18 +11,20 @@ function out_rect = RelativeToRect(rel_x_pos, rel_y_pos, ...
        error('Must specify the scale of at least one dimension.')
     end
 
-    if isempty(rel_x_scale)
-       % assign dims from y
-       y_size = rel_y_scale * (reference_rect(4) - reference_rect(2));
-       x_size = y_size;
-    elseif isempty(rel_y_scale)
-       % assign dims from x
-       x_size = rel_x_scale * (reference_rect(3) - reference_rect(1));
-       y_size = x_size;
-    else
-       x_size = rel_x_scale * (reference_rect(3) - reference_rect(1));
-       y_size = rel_y_scale * (reference_rect(4) - reference_rect(2));
+    y_size = rel_y_scale * (reference_rect(4) - reference_rect(2));
+    x_size = rel_x_scale * (reference_rect(3) - reference_rect(1));
+    if any(isnan(y_size))
+        y_size(isnan(y_size)) = x_size(isnan(y_size));
     end
+    if any(isnan(x_size))
+        x_size(isnan(x_size)) = y_size(isnan(x_size));
+    end
+
+    if ismember(isnan(y_size), isnan(x_size))
+        % todo: add index at least, for debugging
+        error('One dimension must not be nan!')
+    end
+
     tmprct = [zeros(2, size(x_size, 2)); [x_size; y_size]];
     if all(size(tmprct) == 4)
         [cx, cy] = RectCenter(tmprct);
