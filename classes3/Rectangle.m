7dd3a6d7775c710ee53d69458ed298752de56226
aforren1 <aforren1@jhu.edu>
2016-08-17 22:18:54 -0400

    Make sure ShapeTexture is doing the lion's share, notes on implementation tomorrow

--- classes3/Rectangle.m
+++ classes3/Rectangle.m
@@ -4,14 +4,13 @@ classdef Rectangle < ShapeTexture
             self = self@ShapeTexture;
         end
 
-        function Register(self)
-            self.window_pointer = win_pointer;
-
-        end
-
-        function Prime(self)
-            Prime@ShapeTexture(self);
-
+        function Register(self, win_pointer)
+            Register@ShapeTexture(self, win_pointer);
+            Screen('FillRect', self.proto_pointers(1), ...
+                   [255 255 255], Screen('Rect', self.proto_pointer(1)));
+            Screen('FrameRect', self.proto_pointers(2), ...
+                  [255 255 255], Screen('Rect', self.proto_pointer(2)), ...
+                  self.frame_stroke(1));
         end
     end
 end
--- classes3/ShapeTexture.m
+++ classes3/ShapeTexture.m
@@ -22,13 +22,14 @@ classdef (Abstract) ShapeTexture < Texture
                                  all(all(~isnan(x(:, ~isnan(x(1,:)))))));
             self.p.addParamValue('fill_alpha', 255, @(x) all(x >= 0) && all(x <= 255));
             self.p.addParamValue('frame_alpha', 255, @(x) all(x >= 0) && all(x <= 255));
-            self.p.addParamValue('frame_stroke', 5, @(x) all(x >= 0));
+            self.p.addParamValue('frame_stroke', 10, @(x) all(x >= 0));
 
             self.draw_struct.fill_color = [];
             self.draw_struct.frame_color = [];
             self.draw_struct.image_pointer = [];
             self.draw_struct.fill_alpha = [];
             self.draw_struct.frame_alpha = [];
+            self.proto_pointer = [];
         end
 
         function Add(self, indices, varargin)
@@ -36,9 +37,31 @@ classdef (Abstract) ShapeTexture < Texture
             Add@PluralPsychMethods(self, indices, cell_matching, varargin);
         end
 
+        function Register(self, win_pointer)
+            self.window_pointer = win_pointer;
+
+            % re-do textures (in case the pointers are from a previous session)
+            if ~isempty(self.proto_pointer)
+                Screen('Close', self.proto_pointer);
+            end
+
+            self.proto_pointer(1) = Screen('OpenOffscreenWindow', win_pointer, ...
+                                           [0 0 0 0], [0 0 512 512]);
+            self.proto_pointer(2) = Screen('OpenOffscreenWindow', win_pointer, ...
+                                           [0 0 0 0], [0 0 512 512]);
+
+        end
+
+
         function Prime(self)
             Prime@Texture(self);
-            % add to the rest of the struct for drawing & do sorting
+            % reshape things to allow correct drawing order
+            self.draw_struct.draw_rect = zeros();
+            self.draw_struct.rotation_angle = zeros();
+            self.draw_struct.filter_mode = zeros();
+            self.draw_struct.fill_color = zeros(); % add on alphas!!
+            self.draw_struct.frame_color = zeros();
+            self.draw_struct.image_pointer = zeros();
         end
     end
 end
