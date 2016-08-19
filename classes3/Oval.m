b525c224175806afff17daa9d2c08b983ac2048d
aforren1 <aforren1@jhu.edu>
2016-08-19 15:30:08 -0400

    Fixed up rectangle & oval

--- classes3/Oval.m
+++ classes3/Oval.m
@@ -1,14 +1,14 @@
-classdef Rectangle < ShapeTexture
+classdef Oval < ShapeTexture
     methods
-        function self = Rectangle()
+        function self = Oval()
             self = self@ShapeTexture;
         end
 
         function Register(self, win_pointer)
             Register@ShapeTexture(self, win_pointer);
-            Screen('FillRect', self.proto_pointer(1), ...
+            Screen('FillOval', self.proto_pointer(1), ...
                    [255 255 255 255], Screen('Rect', self.proto_pointer(1)));
-            Screen('FrameRect', self.proto_pointer(2), ...
+            Screen('FrameOval', self.proto_pointer(2), ...
                   [255 255 255 255], Screen('Rect', self.proto_pointer(2)), ...
                   self.frame_stroke(1));
         end
--- classes3/ShapeTexture.m
+++ classes3/ShapeTexture.m
@@ -22,7 +22,7 @@ classdef (Abstract) ShapeTexture < Texture
                                  all(all(~isnan(x(:, ~isnan(x(1,:)))))));
             self.p.addParamValue('fill_alpha', 255, @(x) all(x >= 0) && all(x <= 255));
             self.p.addParamValue('frame_alpha', 255, @(x) all(x >= 0) && all(x <= 255));
-            self.p.addParamValue('frame_stroke', 10, @(x) all(x >= 0));
+            self.p.addParamValue('frame_stroke', 15, @(x) all(x >= 0));
 
             self.draw_struct.color = [];
             self.draw_struct.image_pointer = [];
@@ -31,7 +31,18 @@ classdef (Abstract) ShapeTexture < Texture
 
         function Add(self, indices, varargin)
             cell_matching = {'fill_color', 'frame_color'};
-            Add@PluralPsychMethods(self, indices, cell_matching, varargin);
+            self.p.parse(varargin{:});
+            opts = self.p.Results;
+            for fns = fieldnames(opts)'
+                if any(strcmp(fns, cell_matching));
+                    self.(fns{1})(:, indices) = opts.(fns{1});
+                else
+                    self.(fns{1})(indices) = opts.(fns{1});
+                end
+
+            end
+            self.modified = unique([self.modified, indices]);
+            %Add@PluralPsychMethods(self, indices, cell_matching, varargin);
         end
 
         function Register(self, win_pointer)
@@ -46,12 +57,18 @@ classdef (Abstract) ShapeTexture < Texture
                                            [0 0 0 0], [0 0 512 512]);
             self.proto_pointer(2) = Screen('OpenOffscreenWindow', win_pointer, ...
                                            [0 0 0 0], [0 0 512 512]);
+            % draw shapes here
 
         end
 
 
         function Prime(self)
             Prime@Texture(self);
+            if IsOctave
+                tmpfun = @repelem2;
+            else
+                tmpfun = @repelem;
+            end
             % reshape things to allow correct drawing order
             tmp_size = size(self.drawing_rect, 2) * 2;
             self.draw_struct.draw_rect = zeros(4, tmp_size);
@@ -63,10 +80,10 @@ classdef (Abstract) ShapeTexture < Texture
             % subset based on modified AND nans in fill/frame_color
             self.draw_struct.draw_rect(1:4, 1:2:end) = self.drawing_rect;
             self.draw_struct.draw_rect(1:4, 2:2:end) = self.drawing_rect;
-            self.draw_struct.rotation_angle(1, :) = self.rotation_angle(self.modified);
-            self.draw_struct.filter_mode(1, :) = self.filter_mode(self.modified);
-            self.draw_struct.color(1:3, 1:2:end) = self.fill_color(self.modified);
-            self.draw_struct.color(1:3, 2:2:end) = self.frame_color(self.modified);
+            self.draw_struct.rotation_angle(1, :) = tmpfun(self.rotation_angle(self.modified), 2);
+            self.draw_struct.filter_mode(1, :) = tmpfun(self.filter_mode(self.modified), 2);
+            self.draw_struct.color(1:3, 1:2:end) = self.fill_color(:, self.modified);
+            self.draw_struct.color(1:3, 2:2:end) = self.frame_color(:, self.modified);
             self.draw_struct.color(4, 1:2:end) = self.fill_alpha(self.modified);
             self.draw_struct.color(4, 2:2:end) = self.frame_alpha(self.modified);
             self.draw_struct.image_pointer(1, 1:2:end) = self.proto_pointer(1);
@@ -77,23 +94,30 @@ classdef (Abstract) ShapeTexture < Texture
         function Draw(self, indices)
             tmpstrct = self.draw_struct;
             % figure out which indices are not missing
-            indices2 = logical(~(isnan(tmpstrct.fill_color(1, :)) + ...
-                          isnan(tmpstrct.frame_color(1, :))));
+            %indices2 = logical(~(isnan(tmpstrct.fill_color(1, :)) + ...
+            %              isnan(tmpstrct.frame_color(1, :))));
+            indices2 = isnan(tmpstrct.color(1,:));
             % Subset based on requested indices first
-            indices = sort([indices, indices + 1]);
-            tmpstrct.draw_rect(1:4, indices) = [];
-            tmpstrct.rotation_angle(1, indices) = [];
-            tmpstrct.filter_mode(1, indices) = [];
-            tmpstrct.color(1:4, indices) = [];
-            tmpstrct.image_pointer(1, indices) = [];
+            if length(indices) == 1
+                indices = [indices, indices + 1];
+            else
+                indices = [indices, indices + length(indices)];
+            end
+            tmpstrct.draw_rect = tmpstrct.draw_rect(1:4, indices);
+            tmpstrct.rotation_angle = tmpstrct.rotation_angle(1, indices);
+            tmpstrct.filter_mode = tmpstrct.filter_mode(1, indices);
+            tmpstrct.color = tmpstrct.color(1:4, indices);
+            tmpstrct.image_pointer = tmpstrct.image_pointer(1, indices);
 
-            tmpstrct.draw_rect(1:4, indices2) = [];
-            tmpstrct.rotation_angle(1, indices2) = [];
-            tmpstrct.filter_mode(1, indices2) = [];
-            tmpstrct.color(1:4, indices2) = [];
-            tmpstrct.image_pointer(1, indices2) = [];
+            if any(indices2 ~= 0)
+                tmpstrct.draw_rect(:, indices2) = [];
+                tmpstrct.rotation_angle(:, indices2) = [];
+                tmpstrct.filter_mode(:, indices2) = [];
+                tmpstrct.color(:, indices2) = [];
+                tmpstrct.image_pointer(:, indices2) = [];
+            end
 
-            Screen('DrawTextures', tmpstrct.image_pointer, ...
+            Screen('DrawTextures', self.window_pointer, ...
                    tmpstrct.image_pointer, [], ...
                    tmpstrct.draw_rect, ...
                    tmpstrct.rotation_angle, ...
@@ -105,5 +129,29 @@ classdef (Abstract) ShapeTexture < Texture
             Screen('Close', self.proto_pointer);
             delete(self);
         end
+        
+        function Set(self, indices, varargin)
+            % set cell_matching here!
+            cell_matching = {'fill_color', 'frame_color'};
+            if isempty(indices)
+                indices = self.modified;
+                message('Indices unspecified, setting identical values for all...')
+            end
+            if ~any(indices == self.modified)
+                error('Tried to modify a non-existant entry!');
+            end
+            self.p.parse(varargin{:});
+            opts = self.p.Results;
+            temp_names = fieldnames(opts)';
+            delta_names = temp_names(~ismember(temp_names,...
+                                     self.p.UsingDefaults));
+            for fns = delta_names
+                if any(strcmp(fns, cell_matching))
+                    self.(fns{1})(:, indices) = opts.(fns{1});
+                else
+                    self.(fns{1})(indices) = opts.(fns{1});
+                end
+            end
+        end % end Set
     end
 end
